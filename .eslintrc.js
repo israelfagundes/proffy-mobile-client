c1a35c806f99d58a8da7c1e8839f66e2fa2aaa86
Israel <israel.fagundes2001@gmail.com>
2020-08-06 19:20:58 -0300

    initial config with eslint/prettier/editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    es2020: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
